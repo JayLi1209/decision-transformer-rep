92a4b5c56554a951e2ca02e3cf193f48e94b6a40
Yuanhe Li <yli113@wm.edu>
2025-11-27 16:37:42 -0500

    finished most

--- fixed_replay_buffer.py
+++ fixed_replay_buffer.py
@@ -18,7 +18,7 @@ class FixedReplayBuffer(object):
         self._data_dir = data_dir
         self._loaded_buffers = False
         self.add_count = np.array(0)
-        self._replay_suffix = replay_suffix
+        self._replay_suffix = replay_suffix # which replay buffer to load
         if not self._loaded_buffers:
             if replay_suffix is not None:
                 assert replay_suffix >= 0, 'Please pass a non-negative replay suffix!'
@@ -31,16 +31,8 @@ class FixedReplayBuffer(object):
         if replay_buffer is not None:
             self._replay_buffers = [replay_buffer]
             self.add_count = replay_buffer.add_count
-
-    
-
-
-
-
-
-
-
-
+            self._num_replay_buffers = 1
+            self._loaded_buffers = True
 
 
     def _load_buffer(self, suffix):
@@ -57,3 +49,51 @@ class FixedReplayBuffer(object):
         except tf.errors.NotFoundError:
             return None
     
+    def _load_replay_buffers(self, num_buffers=None):
+        '''Loads several checkpoints into a list of OutOfGraphReplayBuffers'''
+        if not self._loaded_buffers:
+            ckpts = gfile.ListDirectory(self._data_dir)
+            # FIXME: The author assumes that the checkpoints are saved in a format CKPT_NAME.{SUFFIX}.gz
+            ckpt_counters = collections.Counter(
+                [name.split('.')[-2] for name in ckpts])
+            # Should contain the files for add_count, action, observation, reward, terminal and invalid_range
+            ckpt_suffixes = [x for x in ckpt_counters if ckpt_counters[x] in [6, 7]]
+            if num_buffers is not None:
+                ckpt_suffixes = np.random.choice(
+                    ckpt_suffixes, num_buffers, replace=False)
+            self._replay_buffers = []
+            # Load the replay buffers in parallel
+            with futures.ThreadPoolExecutor(
+                max_workers=num_buffers) as thread_pool_executor:
+                replay_futures = [thread_pool_executor.submit(
+                    self._load_buffer, suffix) for suffix in ckpt_suffixes]
+            for f in replay_futures:
+                replay_buffer = f.result()
+                if replay_buffer is not None:
+                self._replay_buffers.append(replay_buffer)
+                self.add_count = max(replay_buffer.add_count, self.add_count)
+            self._num_replay_buffers = len(self._replay_buffers)
+            if self._num_replay_buffers:
+                self._loaded_buffers = True
+
+    def get_transition_elements(self):
+        return self._replay_buffers[0].get_transition_elements()
+
+    def sample_transition_batch(self, batch_size=None, indices=None):
+        buffer_index = np.random.randint(self._num_replay_buffers)
+        return self._replay_buffers[buffer_index].samople_transition_batch(
+            batch_size=batch_size, indices=indices
+        )
+    
+    def load(self, *args, **kwargs):
+        pass
+
+    def reload_buffer(self, num_buffers=None):
+        self._loaded_buffers = False
+        self._load_replay_buffers(num_buffers)
+
+    def save(self, *args, **kwargs):
+        pass
+
+    def add(self, *args, **kwargs):
+        pass
--- run_atari.py
+++ run_atari.py
@@ -46,5 +46,31 @@ class StateActionReturnDataset(Dataset):
     
     def __len__(self):
         return len(self.data) - self.block_size
-
     
+    def __get_item__(self, idx):
+        block_size = self.block_size // 3
+        done_idx = idx + block_size
+        for i in self.done_idx:
+            if i > idx:
+                done_idx = min(int(i), done_idx)
+                break
+        idx = done_idx - block_size
+        states = torch.tensor(np.array(self.data[idx:done_idx]), dtype=torch.float32).reshape(block_size, -1) # (block_size, 4*84*84)
+        states = states / 255
+        actions = torch.tensor(self.actions[idx:done_idx], dtype=torch.long).unsqueeze(1) # (block_size, 1)
+        rtgs = torch.tensor(self.rtgs[idx:done_idx], dtype=torch.float32).unsqueeze(1)
+        timesteps = torch.tensor(self.timesteps[idx:idx+1], dtype=torch.int64).unsqueeze(1)
+
+        return states, actions, rtgs, timesteps
+
+obss, actions, returns, done_idx, rtgs, timesteps = create_dataset(args.num_buffers, args.num_steps, args.game, args.data_dir_prefix, args.trajectories_per_buffer)
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
+
+train_dataset = StateActionReturnDataset(obss, args.context_length*3, actions, done_idxs, rtgs, timesteps)
+
+# mconf = GPTConfig
